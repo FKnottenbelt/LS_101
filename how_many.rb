ee414aa7a652a6fbd5227b05e9e9f78aaa1a1a9b
Femke Knottenbelt <femknotgithub@gmail.com>
2018-02-10 17:42:46 +0000

    Add problem How Many?

---
+++ how_many.rb
@@ -0,0 +1,38 @@
+# How Many
+
+# Write a method that counts the number of occurrences of each element in a given array.
+
+# vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck', 'motorcycle', 'motorcycle'
+# , 'car', 'truck']
+
+# count_occurrences(vehicles)
+
+# Once counted, print each element alongside the number of occurrences.
+
+# Expected output:
+
+# car => 4
+# truck => 3
+# SUV => 1
+# motorcycle => 2
+
+####
+# input: array with strings that have doubles
+# output: list value and number of occurrences
+# datatype: array
+# flow:
+#   - make a singles array with unique vehicles
+#   - for each single print the singel and a count that single in the vehicles
+
+vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck', 'motorcycle', 'motorcycle',
+  'car', 'truck']
+ 
+def count_occurrences(vehicles)
+  vehicles.uniq.each {|v| puts "#{v} => #{vehicles.count(v)}"}
+end
+  
+count_occurrences(vehicles)
+# car => 4
+# truck => 3
+# SUV => 1
+# motorcycle => 2
