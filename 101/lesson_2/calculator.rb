1864afce072c63e0038aaba4248e7691ae173008
Femke Knottenbelt <femknotgithub@gmail.com>
2018-02-12 17:25:33 +0000

    Modified calculator for assignment 11 and used Rubocob on it

--- 101/lesson_2/calculator.rb
+++ 101/lesson_2/calculator.rb
@@ -3,12 +3,13 @@
 # Build a command line calculator program that does the following:
 
 #     asks for two numbers
-#     asks for the type of operation to perform: add, subtract, multiply or divide
+#     asks for the type of operation to perform: add, subtract, multiply or
+#     divide
 #     displays the result
 
-# Use the Kernel.gets() method to retrieve user input, and use Kernel.puts() method 
-# to display output. Remember that Kernel.gets() includes the newline, so you have 
-# to call chomp() to remove it: Kernel.gets().chomp().
+# Use the Kernel.gets() method to retrieve user input, and use Kernel.puts()
+# method to display output. Remember that Kernel.gets() includes the newline,
+# so you have to call chomp() to remove it: Kernel.gets().chomp().
 ##########
 
 # flow:
@@ -17,27 +18,100 @@
 # perform the operation on the 2 numbers
 # output the result
 
-Kernel.puts("Welcome to Calculator!")
-
-Kernel.puts("What is the first number?")
-number1 = Kernel.gets().chomp()
-
-Kernel.puts("What is the second number?")
-number2 = Kernel.gets().chomp()
-
-Kernel.puts("what operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-operator = Kernel.gets().chomp()
-
-if operator == '1'
-  result = number1.to_i() + number2.to_i()
-elsif operator == '2'
-  result = number1.to_i() - number2.to_i()
-elsif operator == '3'
-  result = number1.to_i() * number2.to_i()
-elsif operator == '4'
-  result = number1.to_f() / number2.to_f()
-else
-  Kernel.puts("sorry, invalid input")
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
-Kernel.puts("The result is #{result}")
+def valid_number?(num)
+  num.to_i != 0
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+
+prompt("Welcome to Calculator! Enter your name:")
+name = ''
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?
+    prompt("Make sure to use a valid name")
+  else
+    break
+  end
+end
+
+prompt("Hello #{name}")
+loop do # main
+  number1 = ''
+  loop do
+    prompt("What is the first number?")
+    number1 = Kernel.gets().chomp()
+
+    if valid_number?(number1)
+      break
+    else
+      prompt("Hmm...that doesn't look like a valid number")
+    end
+  end
+
+  number2 = ''
+  loop do
+    prompt("What is the second number?")
+    number2 = Kernel.gets().chomp()
+    if valid_number?(number2)
+      break
+    else
+      prompt("Hmm...that doesn't look like a valid number")
+    end
+  end
+
+  operator_prompt = <<-MSG
+    what operation would you like to perform?
+      1) add
+      2) subtract
+      3) multiply
+      4) divide
+  MSG
+  prompt(operator_prompt)
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Please choose 1, 2, 3 or 4")
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result =  case operator
+            when '1'
+              number1.to_i() + number2.to_i()
+            when '2'
+              number1.to_i() - number2.to_i()
+            when '3'
+              number1.to_i() * number2.to_i()
+            when '4'
+              number1.to_f() / number2.to_f()
+            end
+
+  prompt("The result is #{result}")
+  prompt("Do you want to perform another calculation? Y to calculate again")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for using the calculator. Good Bye!")
