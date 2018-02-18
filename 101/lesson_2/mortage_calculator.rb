f4c2494f2f687efd1c965e75aa695dbcd5e89309
Femke Knottenbelt <femknotgithub@gmail.com>
2018-02-18 22:20:01 +0000

    Refactoring Mortage Calculator

--- 101/lesson_2/mortage_calculator.rb
+++ 101/lesson_2/mortage_calculator.rb
@@ -11,6 +11,7 @@
 #     j = monthly interest rate (MPR)
 #     n = loan duration in months
 # d: numbers
+# example: mortage_calculator(100,1,1) => $8.38 for 12 months
 # f: - welcome
 #   - ask for input
 #   - validate input
@@ -21,52 +22,65 @@
 #   - ask if we want to go again
 #   - if not, say goodbye
 
-def prompt(message)
-  puts(">> " + message)
+require 'yaml'
+LANGUAGE = 'eng' # set to 'eng' for English, 'dutch' for Dutch
+MESSAGES = YAML.load_file('mortage_calculator_messages.yml')
+
+def messages(key, lang = 'eng', variable = '')
+  message = if variable == ''
+              MESSAGES[lang][key]
+            else
+              MESSAGES[lang][key] % variable
+            end
+  message
 end
 
-prompt "Welcome to the mortage caculator!"
+def prompt(key, variable = '')
+  message = messages(key, LANGUAGE, variable)
+  puts("=> #{message}")
+end
+
+def valid_float?(number)
+  true if Float(number)
+rescue
+  false
+end
+
+def get_input(input_prompt, message_prompt)
+  number = 0
+  loop do
+    prompt input_prompt
+    number = gets
+    valid_float?(number) ? number = number.to_f : number
+    break if valid_float?(number) && number > 0
+    prompt message_prompt
+  end
+  number
+end
+
+prompt 'welcome'
 
 loop do # main
-  loan = 0 # p
-  loop do
-    prompt "How much do you want to borrow?"
-    loan = gets.to_f
-    break if Float(loan) && loan > 0
-    prompt "Please enter a valid loan amount greater than 0"
-  end
+  loan = get_input('loan', 'invalid_loan')
+  interest_per_year = get_input('interest', 'invalid_interest')
+  loan_duration_year = get_input('loan_duration', 'invalid_loan_duration')
 
-  interest_per_jear = 0
-  loop do
-    prompt "How much interest are you willing to pay?"
-    interest_per_jear = gets.to_i
-    break if Integer(interest_per_jear) && interest_per_jear > 0
-    prompt "please enter interest as an integer greater then 0"
-  end
-
-  loan_duration_years = 0
-  loop do
-    prompt "How many years do you want to loan?"
-    loan_duration_years = gets.to_i
-    break if Integer(loan_duration_years) && loan_duration_years > 0
-    prompt "please enter loan duration as an integer greater then 0"
-  end
-
-  interest_per_jear = interest_per_jear.to_f / 100
-  interest_per_month = interest_per_jear / 12 # j
-  loan_duration_months = loan_duration_years * 12 # n
+  interest_per_year /= 100
+  interest_per_month = interest_per_year / 12 # j
+  loan_duration_months = loan_duration_year * 12 # n
 
   # caluculate monthly payment as
   # m = p * (j / (1 - (1 + j)**(-n)))
   monthly_payment = loan * (interest_per_month /
     (1 - (1 + interest_per_month)**-loan_duration_months))
 
-  prompt "Your monthly payment will be $#{format('%.2f', monthly_payment)}"
-  prompt "for the duration of #{loan_duration_months} months."
+  prompt 'result_payment', { monthly_payment: format('%.2f', monthly_payment) }
+  prompt 'result_month', { loan_duration_months: loan_duration_months.round(0) }
 
-  prompt "do you want to calculate again? y for yes"
+  prompt 'keep_going?'
   answer = gets.chomp.downcase
-  break unless answer == 'y' || answer == 'yes' || answer == 'yeah'
+  break unless %w(y yeah yes).include?(answer)
+  system('clear') || system('cls')
 end # main
 
-prompt "Thank you for using the mortage calculator. Goodbye!"
+prompt 'exit'
