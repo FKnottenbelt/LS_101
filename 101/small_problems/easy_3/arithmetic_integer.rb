6c861d7c9443ef52ec05570cfe8ad49483ece839
Femke Knottenbelt <femknotgithub@gmail.com>
2018-02-19 20:28:21 +0000

    Add problem Arithmetic Integer

---
+++ 101/small_problems/easy_3/arithmetic_integer.rb
@@ -0,0 +1,30 @@
+# arithmetic_integer.rb
+
+# Write a program that prompts the user for two positive integers, and then
+# prints the results of the following operations on those two numbers:
+# addition, subtraction, product, quotient, remainder, and power. Do not worry
+# about validating the input.
+
+# Example
+
+# ==> Enter the first number:
+# 23
+# ==> Enter the second number:
+# 17
+# ==> 23 + 17 = 40
+# ==> 23 - 17 = 6
+# ==> 23 * 17 = 391
+# ==> 23 / 17 = 1
+# ==> 23 % 17 = 6
+# ==> 23 ** 17 = 141050039560662968926103
+
+puts "==> Enter the first number:"
+num1 = gets.to_i
+puts " ==> Enter the second number:"
+num2 = gets.to_i
+puts "==> #{num1} + #{num2} = #{num1 + num2}"
+puts "==> #{num1} - #{num2} = #{num1 - num2}"
+puts "==> #{num1} * #{num2} = #{num1 * num2}"
+puts "==> #{num1} / #{num2} = #{num1 / num2}"
+puts "==> #{num1} % #{num2} = #{num1 % num2}"
+puts "==> #{num1} **+ #{num2} = #{num1 ** num2}"
