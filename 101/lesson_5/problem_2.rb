7bb562c12c8eab2efbecd0f06c8490e46c0becee
Femke Knottenbelt <femknotgithub@gmail.com>
2018-03-21 20:14:40 +0000

    Add lesson 5 problem 3

--- 101/lesson_5/problem_2.rb
+++ 101/lesson_5/problem_2.rb
@@ -10,21 +10,18 @@ books = [
   {title: 'Ulysses', author: 'James Joyce', published: '1922'}
 ]
 
-
-out1 = books.sort_by { |_,_,date| date }
-
-
-people = { Kate: 27, john: 25, Mike:  18 }
-sorted_people =
-people.sort_by do |name, age|
-  name.capitalize
-end
-puts sorted_people
-# => [[:john, 25], [:Kate, 27], [:Mike, 18]]
-
 output =
 books.sort_by do |book|
   book[:published]
 end
 
 puts output
+
+# notes:
+# We can use sort_by and access a particular value in each hash by which to
+# sort, as long as those values have a <=> method.
+
+# In this case, all the values for :published are strings so we know that we
+# can compare them. Since all the values in question are four characters in
+# length, in this case we can simply compare the strings without having to
+# convert them to integers.
