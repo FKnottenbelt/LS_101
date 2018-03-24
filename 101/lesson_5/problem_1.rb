a22f5f80948b07ef687c18220153b317cf1a449d
Femke Knottenbelt <femknotgithub@gmail.com>
2018-03-24 20:07:25 +0000

    Add problem 1 take 2

--- 101/lesson_5/problem_1.rb
+++ 101/lesson_5/problem_1.rb
@@ -11,6 +11,13 @@
 
 arr = ['10', '11', '9', '7', '8']
 
-arr.sort! { |n1, n2| n2.to_i <=> n1.to_i }
-
+out = arr.sort { |n1, n2| n2.to_i <=> n1.to_i}
+# or: arr.sort! { |n1, n2| n2.to_i <=> n1.to_i}
 p arr
+p out
+
+#LS solution
+arr.sort do |a,b|
+  b.to_i <=> a.to_i
+end
+# => ["11", "10", "9", "8", "7"]
