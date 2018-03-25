e3f59d47b184bf5e063ee6e43f2a727c8aae15a9
Femke Knottenbelt <femknotgithub@gmail.com>
2018-03-25 08:33:38 +0000

    Add problem 8 take 2

--- 101/lesson_5/problem_8.rb
+++ 101/lesson_5/problem_8.rb
@@ -6,10 +6,20 @@
 hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'],
        fourth: ['over', 'the', 'lazy', 'dog']}
 
+# option 1
 VOWELS = ['a','e','i','o','u']
 
 hsh.each_value do |words|
   words.join().chars { |l| puts l if VOWELS.include?(l)}
+end
+
+# option 2
+hsh.each_value do |words_array|
+  words_array.each do |words|
+    words.each_char do |char|
+      print char if VOWELS.include?(char)
+    end
+  end
 end
 
 # ls solution
