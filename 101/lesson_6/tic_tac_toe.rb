0f623706d0268eceeba29fb10d730f18a5885b88
Femke Knottenbelt <femknotgithub@gmail.com>
2018-03-30 15:19:35 +0000

    Refactor Tic Tac Toe with rubocop

--- 101/lesson_6/tic_tac_toe.rb
+++ 101/lesson_6/tic_tac_toe.rb
@@ -16,7 +16,7 @@
 # Step 3: The main game loop (done)
 # Step 4: Determining the winner (done)
 # Step 5: Play again (done)
-
+# Step 6: refactor with rubocop done
 
 require 'pry'
 
@@ -28,11 +28,11 @@ WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                 [[1, 5, 9], [3, 5, 7]]              # diagonals
 
-
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+# rubocop: disable Metrics/AbcSize
 def display_board(brd)
   system 'clear' || system(cls)
   puts "You are a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
@@ -50,15 +50,16 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
+# rubocop: enable Metrics/AbcSize
 
 def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = INITIAL_MARKER}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def empty_squares(brd)
-  brd.keys.select{ |num| brd[num] == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_places_piece!(brd)
@@ -82,22 +83,14 @@ def board_full?(brd)
 end
 
 def detect_winner(brd)
-  WINNING_LINES.each do |lines|
-    if brd[lines[0]] == PLAYER_MARKER &&
-       brd[lines[1]] == PLAYER_MARKER &&
-       brd[lines[2]] == PLAYER_MARKER
-       return 'Player'
-    elsif brd[lines[0]] == COMPUTER_MARKER &&
-          brd[lines[1]] == COMPUTER_MARKER &&
-          brd[lines[2]] == COMPUTER_MARKER
-          return 'Computer'
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return "Player"
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return "Computer"
     end
   end
   nil
-end
-
-def someone_won?(brd)
-  !!detect_winner(brd)
 end
 
 def someone_won?(brd)
