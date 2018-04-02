6ab553485d049c1715539998e4e49f043427311d
Femke Knottenbelt <femknotgithub@gmail.com>
2018-04-02 11:54:31 +0000

    Add Twenty-One Initialize deck and Deal cards to player and dealer

---
+++ 101/lesson_6/twenty_one.rb
@@ -0,0 +1,58 @@
+# twenty_one.rb
+
+##########################
+# 1. Initialize deck (done)
+# 2. Deal cards to player and dealer (done)
+# 3. Player turn: hit or stay
+#   - repeat until bust or "stay"
+# 4. If player bust, dealer wins.
+# 5. Dealer turn: hit or stay
+#   - repeat until total >= 17
+# 6. If dealer bust, player wins.
+# 7. Compare cards and declare winner.
+
+def initialize_unused_cards!
+  cards = []
+  # add number values for 4 suites
+  counter = 2
+  loop do
+    break if counter == 11
+    4.times { cards << counter }
+    counter += 1
+  end
+
+  # add picture cards for 4 suites
+  cards << ['jack'] * 4
+  cards << ['queen'] * 4
+  cards << ['king'] * 4
+  cards << ['ace'] * 4
+  cards.flatten!
+end
+
+def initialize_deck!(deck)
+  deck[:deck] = initialize_unused_cards!
+  deck[:player] = []
+  deck[:dealer] = []
+  deck
+end
+
+def deal(deck, number_of_cards_to_deal, *current_player)
+  current_player.each do |player|
+    number_of_cards_to_deal.times do
+      card = deck[:deck].sample
+      deck[:deck].delete_at(deck[:deck].index(card))
+      deck[player.to_sym] << card
+    end
+  end
+end
+
+#####################################
+deck = initialize_deck!({})
+
+number_of_cards_to_deal = 2
+deal(deck, number_of_cards_to_deal, 'player', 'dealer')
+
+number_of_cards_to_deal = 1
+deal(deck, number_of_cards_to_deal, 'player')
+
+p deck
