ae6bb5139705ee16a16144c4a98495fec124edbc
Femke Knottenbelt <femknotgithub@gmail.com>
2018-04-09 20:32:08 +0000

    Add problem Multiplicative Average

---
+++ 101/small_problems/easy_7/multiplicative_average.rb
@@ -0,0 +1,26 @@
+# multiplicative_average.rb
+
+# Write a method that takes an Array of integers as input, multiplies
+# all the numbers together, divides the result by the number of entries
+# in the Array, and then prints the result rounded to 3 decimal places.
+# Assume the array is non-empty.
+
+# i: array with integers
+# o: string The result is 7.500 rounded to 3 decimal places
+# f: multiplies all the integers: so reduce(:*)
+#   then take the result and divide bij count of array
+#   than print rounded to 3 decimals
+
+def show_multiplicative_average(array)
+  result = array.reduce(:*).to_f / array.count
+  puts "The result is #{format('%.3f', result)}"
+end
+
+show_multiplicative_average([3, 5])
+#The result is 7.500
+
+show_multiplicative_average([6])
+#The result is 6.000
+
+show_multiplicative_average([2, 5, 7, 11, 13, 17])
+#The result is 28361.667
