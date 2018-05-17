18373a167b15cf03d3bb90cfd2e718e90c5d6c99
Femke Knottenbelt <femknotgithub@gmail.com>
2018-05-17 09:01:29 +0000

    Add file with video problem sets

---
+++ 109/code_problems/video_questions.rb
@@ -0,0 +1,36 @@
+# Video questions:
+
+watch-others-code-recording-part-1
+1a - calculating primes 4:11
+write a method that will determine whether an integer is a prime.
+Don't use the prime class.
+p is_prime?(3) == true
+p is_prime?(4) == false
+
+1b:
+Write a method that will take an array of numbers and only return those that
+are prime
+
+p select_primes([1, 2, 3, 4]) == [2,3]
+p select_primes([4, 6, 8, 10]) == []
+
+1c: (ends 25:45)
+write a method that will take an array of numbers, and return the number of
+primes in the array
+
+p count_primes([1, 2, 3, 4]) == 2
+p count_primes([4, 6, 8, 10]) == 0
+
+2- finding sum or product of a series of numbers
+
+interleave two arrays
+
+watch-others-code-recording-part-4
+calculate number of Friday the 13ths in a year
+generate random English mathematical phrases
+
+
+beginning-ruby-part-4
+
+
+beginning-ruby-part-5
