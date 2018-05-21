d779999fd0f2e800b5d8644e19d335250700e0b9
Femke Knottenbelt <femknotgithub@gmail.com>
2018-05-21 07:18:48 +0000

    Add more practice questions for review

--- 109/code_problems/lesson5_questions.rb
+++ 109/code_problems/lesson5_questions.rb
@@ -57,3 +57,62 @@ munsters = {
 # ...like this:
 
 # (Name) is a (age)-year-old (male or female).
+
+# problem_8.rb
+
+# Using the each method, write some code to output all of the vowels from the
+# strings
+
+hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'],
+       fourth: ['over', 'the', 'lazy', 'dog']}
+
+# problem_9.rb
+
+# Given this data structure, return a new array of the same structure but with
+# the sub arrays being ordered (alphabetically or numerically as appropriate)
+# in descending order.
+
+arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']]
+
+#=> [["c", "b", "a"], [3, 2, 1], ["green", "blue", "black"]]
+
+# problem_10.rb
+
+# Given the following data structure and without modifying the original array,
+# use the map method to return a new array identical in structure to the
+# original but where the value of each integer is incremented by 1.
+
+arr = [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}]
+# => [{:a=>2}, {:b=>3, :c=>4}, {:d=>5, :e=>6, :f=>7}]
+
+# problem_11.rb
+
+# Given the following data structure use a combination of methods, including
+# either the select or reject method, to return a new array identical in
+# structure to the original but containing only the integers that are
+# multiples of 3.
+
+arr = [[2], [3, 5, 7], [9], [11, 13, 15]]
+# => [[], [3], [9], [15]]
+
+# problem_12.rb
+
+# Given the following data structure, and without using the Array#to_h method,
+# write some code that will return a hash where the key is the first item in
+# each sub array and the value is the second item.
+
+arr = [[:a, 1], ['b', 'two'], ['sea', {c: 3}], [{a: 1, b: 2, c: 3, d: 4}, 'D']]
+
+# => {:a=>1, "b"=>"two", "sea"=>{:c=>3}, {:a=>1, :b=>2, :c=>3, :d=>4}=>"D"}
+
+# problem_13.rb
+
+# Given the following data structure, return a new array containing the same
+# sub-arrays as the original but ordered logically according to the numeric
+# value of the odd integers they contain.
+
+arr = [[1, 6, 7], [1, 4, 9], [1, 8, 3]]
+
+# The sorted array should look like this:
+
+# [[1, 8, 3], [1, 6, 7], [1, 4, 9]]
